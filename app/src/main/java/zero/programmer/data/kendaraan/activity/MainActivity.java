ee1d490fc23f6eccc5a4a3fe73f0311e559d2518
bobi1539 <ahmadrival833@gmail.com>
2021-08-03 09:43:23 +0700

    membuat project git

---
+++ app/src/main/java/zero/programmer/data/kendaraan/activity/MainActivity.java
@@ -0,0 +1,71 @@
+package zero.programmer.data.kendaraan.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import zero.programmer.data.kendaraan.R;
+import zero.programmer.data.kendaraan.fragment.BorrowFragment;
+import zero.programmer.data.kendaraan.fragment.DriverFragment;
+import zero.programmer.data.kendaraan.fragment.ProfileFragment;
+import zero.programmer.data.kendaraan.fragment.UserFragment;
+import zero.programmer.data.kendaraan.fragment.VehicleFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(navigationListener);
+
+        // Setting Profile Fragment as main Fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_layout, new ProfileFragment())
+                .commit();
+    }
+
+    // Listener Navigation Bar
+    private NavigationBarView.OnItemSelectedListener navigationListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.item_person:
+                    selectedFragment = new ProfileFragment();
+                    break;
+                case R.id.item_vehicle:
+                    selectedFragment = new VehicleFragment();
+                    break;
+                case R.id.item_create:
+                    selectedFragment = new BorrowFragment();
+                    break;
+                case R.id.item_driver:
+                    selectedFragment = new DriverFragment();
+                    break;
+                case R.id.item_user:
+                    selectedFragment = new UserFragment();
+                    break;
+            }
+
+            // Begin transaction
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_layout, selectedFragment)
+                    .commit();
+
+            return true;
+        }
+    };
+}
